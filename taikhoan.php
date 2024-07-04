361c6bcdcf0c1c421695adc7d58dec99778fd213
anhpv21 <songoku2016z@gmail.com>
2024-07-04 23:39:39 +0700

    push models taikhoan.php

--- taikhoan.php
+++ taikhoan.php
@@ -4,10 +4,4 @@
         $sql = "INSERT INTO tai_khoan(`ten_dang_nhap`, `mat_khau`, `email`, `trang_thai`, `hinh_anh`, `sdt`, `dia_chi`, `vai_tro`) VALUES('$ten_dang_nhap', '$mat_khau', '$email', '$trang_thai', NULL, NULL, NULL, '$vai_tro')";
         pdo_execute($sql);
     }
-
-    // Check tài khoản
-    function check_tai_khoan($email, $mat_khau) {
-        $sql = "SELECT * FROM tai_khoan WHERE email = '$email', mat_khau = '$mat_khau'";
-        pdo_query_one($sql);
-    }
 ?>
