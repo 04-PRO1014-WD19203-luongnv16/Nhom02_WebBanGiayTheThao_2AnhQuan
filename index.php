e4cc9acede67f653614fbed190420e3f4b097a2d
wuanne <148039666+wuan25@users.noreply.github.com>
2024-07-13 14:54:19 +0700

    Update index.php khung index lại

--- index.php
+++ index.php
@@ -19,10 +19,7 @@ if (isset($_GET['act'])) {
             
             break;
         case 'dang_xuat':
-             if(isset($_SESSION['user'])){
-            unset($_SESSION['user']);
-            header('location:index.php');
-        }
+            
             break;
         case 'cua_hang':
             
@@ -31,55 +28,13 @@ if (isset($_GET['act'])) {
             
             break;
         case 'them_vao_gio_hang':
-             if(isset($_POST['them_gio_btn'])){
-        $id_bien_the = $_POST['id_bien_the'];
-        $id_tai_khoan = $_POST['id_tai_khoan'];
-        $so_luong = $_POST['so_luong'];
-        $id_san_pham = $_POST['id_san_pham'];
-
-        // Kiểm tra nếu size chưa được chọn
-        if (empty($_POST['gia_size'])) {
-            $thongbao = "Vui lòng chọn size trước khi thêm vào giỏ hàng.";
-            // Lấy thông tin sản phẩm và size để hiển thị lại trang chi tiết
-            $one_san_pham = show_1_san_pham($id_san_pham);
-            $size = tat_ca_size();
-            include 'view/chitietsanpham.php';
-           
-        } else {
-            // Size đã được chọn, tiếp tục xử lý thêm vào giỏ hàng
-            $gia_size = $_POST['gia_size'];
-            $tong_gia = $gia_size * $so_luong;
-            $check = kiem_tra_ton_tai_san_pham_trong_gio_hang($id_bien_the, $id_tai_khoan);
-            if($check !== false){
-                $so_luong_cu = $check['so_luong'];
-                $so_luong_moi = $so_luong_cu + $so_luong;
-                $tong_gia = $gia_size * $so_luong_moi;
-                cap_nhat_so_luong_gio_hang($id_bien_the, $id_tai_khoan, $so_luong_moi, $tong_gia);
-            } else {
-                them_vao_gio_hang($id_bien_the, $id_tai_khoan, $so_luong, $tong_gia);
-            }
-          
-            header('location:index.php?act=view_gio_hang');
-            exit; 
-        }
-    }     
+            
             break;
         case 'view_gio_hang':
-             if(isset($_SESSION['user']['id_tai_khoan'])){
-                $id_tai_khoan = $_SESSION['user']['id_tai_khoan'];
-                 $gio_hang =   show_gio_hang($id_tai_khoan);
-            }
-            include 'view/viewgiohang.php';
+            
             break;
         case 'xoa_toan_bo_gio_hang':
-            $id_bien_the = 0;
-            if(isset($_GET['idxgh'])){
-                $id_bien_the  =$_GET['idxgh'];
-                
-            }
-            xoa_toan_bo_gio_hang($id_tai_khoan,$id_bien_the);
-            $gio_hang =   show_gio_hang($id_tai_khoan);
-            include 'view/viewgiohang.php';
+            
             break;
         default:
             $iddm = 0;
