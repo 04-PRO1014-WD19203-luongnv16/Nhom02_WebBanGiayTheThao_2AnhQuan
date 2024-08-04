a105446c62d7f3a2204b02baf358441ab1735e58
Anhhihi23 <anhdtph46943@fpt.edu.vn>
2024-08-04 22:02:48 +0700

    Update lại các case danhmuc, user,binhluan

--- admin/index.php
+++ admin/index.php
@@ -109,313 +109,37 @@ if(isset($_GET['act'])){
         
         #size-----------
         case 'size':
-            $size = tat_ca_size();
-            include 'view/size/list.php';
             break;
         case 'them_size':
-                if(isset($_POST['them_btn'])) {
-                    $ten_size = $_POST['ten_size'];
-                    
-                    if(empty($ten_size)) {
-                        $thongbao = "Vui lòng nhập size";
-                    } else {
-                        $check =  check_size($ten_size);
-                        
-                        if($check !== false) {
-                            $thongbao = "Size đã tồn tại";
-                        } else {
-                            them_moi_size($ten_size);
-                           
-                        }
-                    }
-                }
-            $size = tat_ca_size();
-            include 'view/size/list.php';
-                break;
+            break;
             case 'xoa_size':
-                    if(isset($_GET['idsz'])){
-                    $idsz = $_GET['idsz'];
-                    xoa_size($idsz);
-                    $size = tat_ca_size();
-                    include 'view/size/list.php';
-                    }
-                    break;
+            break;
             case 'size_da_xoa':
-                    $tat_ca_size_da_xoa = tat_ca_size_da_xoa();
-                    include 'view/size/list_delete.php';
-                    break;     
+                   break;     
             case 'khoi_phuc_size':
-                    if(isset($_GET['id_szdx'])){
-                    $id_szdx = $_GET['id_szdx'];
-                    khoi_phuc_size($id_szdx);
-                    $tat_ca_size_da_xoa = tat_ca_size_da_xoa();
-                    include 'view/size/list_delete.php';
-                    }
                     break;
             case 'khoi_phuc_toan_bo_size':
-                khoi_phuc_toan_bo_size();
-                $tat_ca_size_da_xoa = tat_ca_size_da_xoa();
-                include 'view/size/list_delete.php';
                 break;
             case 'sua_size':
-                if(isset($_GET['idssz'])){
-                    $id_size = $_GET['idssz'];
-                    $one_size =show_1_size($id_size);
-                }
-                include 'view/size/fix.php';
-                break;
+               break;
             case 'update_size':
-                if (isset($_POST['sua_btn'])) {
-                    $id_size = $_POST['id_size'];
-                    $ten_size = $_POST['ten_size'];
-                    $errors = array();
-            
-                    // Validate tên danh mục
-                    if (empty(trim($ten_size))) {
-                        $errors[] = "Không được để trống Size.";
-                    }
-            
-                    // Nếu không có lỗi, cập nhật danh mục
-                    if (empty($errors)) {
-                        sua_size($id_size,$ten_size);
-                      
-                       
-                        header('Location: index.php?act=size');
-                       
-                        exit();
-                    } else {
-                        // Nếu có lỗi, lấy lại dữ liệu danh mục
-                        $one_size =show_1_size($id_size);
-                    }
-                } else {
-                    // Nếu không phải là POST request, lấy thông tin danh mục
-                    if(isset($_GET['idssz'])){
-                        $id_size = $_GET['idssz'];
-                        $one_size =show_1_size($id_size);
-                    }
-                }
-            
-                $size = tat_ca_size();
-                include 'view/size/fix.php';
                 break;   
         #sản phẩm--------
             case 'san_pham':
-                $iddm =0;
-                if(isset($_POST['tim_btn'])){
-                    $iddm = $_POST['danh_muc'];
-
-                }
-                $danh_muc =tat_ca_danh_muc();
-                $san_pham = tat_ca_san_pham($iddm);
-                include 'view/sanpham/list.php';
                 break;
                 case 'them_san_pham':
-                    $dem = 0;
-                    $loi_ten = $loi_danh_muc = $loi_mo_ta = $loi_size = $loi_gia_nhap = $loi_sale = $loi_gia_ban = $loi_so_luong = $loi_anh = "";
-                    
-                    if (isset($_POST['them_btn'])) {
-                        $ten_san_pham = $_POST['ten_san_pham'];
-                        $mo_ta = $_POST['mo_ta'];
-                        $danh_muc = $_POST['danh_muc'];
-                    
-                        # Xử lý validate
-                        if (empty($ten_san_pham)) {
-                            $loi_ten = "Tên sản phẩm không được để trống";
-                            $dem++;
-                        }
-                    
-                        if (empty($mo_ta)) {
-                            $loi_mo_ta = "Mô tả không được để trống";
-                            $dem++;
-                        }
-                    
-                        if ($danh_muc == 0) {
-                            $loi_danh_muc = "Hãy chọn 1 danh mục";
-                            $dem++;
-                        }
-                    
-                        // Validate biến thể sản phẩm
-                        $sizes = $_POST['size'];
-                        $importPrices = $_POST['importPrice'];
-                        $sales = $_POST['Sale'];
-                        $salePrices = $_POST['salePrice'];
-                        $quantities = $_POST['quantity'];
-                    
-                        for ($i = 0; $i < count($sizes); $i++) {
-                            if (empty($sizes[$i])) {
-                                $loi_size = "Hãy chọn size cho biến thể thứ " . ($i + 1);
-                                $dem++;
-                            }
-                            if (empty($importPrices[$i])) {
-                                $loi_gia_nhap = "Giá nhập không được để trống cho biến thể thứ " . ($i + 1);
-                                $dem++;
-                            }
-                            if (empty($sales[$i])) {
-                                $loi_sale = "Giá sale không được để trống cho biến thể thứ " . ($i + 1);
-                                $dem++;
-                            }
-                            if (empty($salePrices[$i])) {
-                                $loi_gia_ban = "Giá bán không được để trống cho biến thể thứ " . ($i + 1);
-                                $dem++;
-                            }
-                            if (empty($quantities[$i])) {
-                                $loi_so_luong = "Số lượng không được để trống cho biến thể thứ " . ($i + 1);
-                                $dem++;
-                            }
-                        }
-                    
-                        // Validate ảnh sản phẩm
-                        $numFiles = count($_FILES['productImages']['name']);
-                        if ($numFiles == 0) {
-                            $loi_anh = "Bạn phải chọn ít nhất một ảnh sản phẩm";
-                            $dem++;
-                        } 
-                    
-                        if ($dem == 0) {
-                            // Thêm sản phẩm
-                            them_san_pham($ten_san_pham, $mo_ta, $danh_muc);
-                            $id_san_pham = tim_idsp();
-                    
-                            // Xử lý biến thể sản phẩm
-                            for ($i = 0; $i < count($sizes); $i++) {
-                                $size = $sizes[$i];
-                                $gia_nhap = $importPrices[$i];
-                                $sale = $sales[$i];
-                                $gia_ban = $salePrices[$i];
-                                $so_luong = $quantities[$i];
-                                
-                                them_bien_the_san_pham($id_san_pham, $size, $gia_nhap, $gia_ban, $sale, $so_luong);
-                            }
-                    
-                            // Xử lý ảnh sản phẩm
-                            $uploadedImages = array();
-                            for ($i = 0; $i < $numFiles; $i++) {
-                                $image_name = $_FILES['productImages']['name'][$i];
-                                $tmp = $_FILES['productImages']['tmp_name'][$i];
-                                $uploadPath = '../uploads/' . $image_name;
-                    
-                                if (move_uploaded_file($tmp, $uploadPath)) {
-                                    $uploadedImages[] = $image_name; // Lưu tên ảnh vào mảng để thêm vào cơ sở dữ liệu sau này
-                                } else {
-                                    echo "Không thể tải lên ảnh $image_name.<br>";
-                                }
-                            }
-                    
-                            // Thêm ảnh vào db
-                            foreach ($uploadedImages as $image) {
-                                them_anh_san_pham($id_san_pham, $image);
-                            }
-                    
-                            // Thông báo thành công
-                            $thongbao = "Thêm sản phẩm thành công";
-                        }
-                    }
-                    
-                    $danh_muc = tat_ca_danh_muc();
-                    $size = tat_ca_size();
-                    include 'view/sanpham/add.php';
-                    break;
-                
-            case 'xoa_san_pham':
-            if(isset($_GET['id_xoasp'])){
-                $id_san_pham= $_GET['id_xoasp'];
-                xoa_san_pham($id_san_pham);
-            }
-            $iddm =0;
-            if(isset($_POST['tim_btn'])){
-                $iddm = $_POST['danh_muc'];
-
-            }
-            $danh_muc =tat_ca_danh_muc();
-            $san_pham = tat_ca_san_pham($iddm);
-            include 'view/sanpham/list.php';
+                   break;
+                case 'xoa_san_pham':
             break;
             case 'san_pham_da_xoa':
-                $san_pham_da_xoa = san_pham_da_xoa();
-            include 'view/sanpham/list_delete.php';
-            break;
+                break;
             case 'khoi_phuc_san_pham':
-                if(isset($_GET['id_spdx'])){
-                    $id_san_pham= $_GET['id_spdx'];
-                    khoi_phuc_san_pham($id_san_pham);
-                }
-                $san_pham_da_xoa = san_pham_da_xoa();
-                include 'view/sanpham/list_delete.php';
-                break;
+               break;
                 case 'chi_tiet_san_pham':
-                    $id_san_pham = isset($_GET['id_sp']) ? $_GET['id_sp'] : null;
-                    $one_san_pham = show_1_san_pham($id_san_pham);
-                    include 'view/sanpham/deltai.php';                  
-                    break;
+                   break;
                 case 'sua_san_pham':
-                if(isset($_GET['id_ssp'])){
-                    $id_san_pham = $_GET['id_ssp'];
-                    $one_san_pham = show_1_san_pham($id_san_pham);
-                    $so_luong_bien_the = count($one_san_pham['sizes']);
-                    extract($one_san_pham);
-                }
-                $danh_muc = tat_ca_danh_muc();
-                $size = tat_ca_size();
-                include 'view/sanpham/fix.php';   
-                break;
+              break;
                 case 'update_san_pham':
-                    if(isset($_POST['sua_btn'])){
-                        $id_san_pham = $_POST['id_san_pham'];
-                        $ten_san_pham = $_POST['ten_san_pham'];
-                        $mo_ta = $_POST['mo_ta'];
-                        $danh_muc = $_POST['danh_muc'];
-                        $sizes = $_POST['size'];
-                        $importPrices = $_POST['importPrice'];
-                        $sales = $_POST['Sale'];
-                        $salePrices = $_POST['salePrice'];
-                        $quantities = $_POST['quantity'];
-                
-                        // Sửa sản phẩm
-                        sua_san_pham($id_san_pham, $ten_san_pham, $mo_ta, $danh_muc);
-                
-                        // Xử lý biến thể sản phẩm
-                        for ($i = 0; $i < count($sizes); $i++) {
-                            $size = $sizes[$i];
-                            $gia_nhap = $importPrices[$i];
-                            $sale = $sales[$i];
-                            $gia_ban = $salePrices[$i];
-                            $so_luong = $quantities[$i];
-                
-                            // Kiểm tra biến thể đã tồn tại chưa
-                            if (bien_the_da_ton_tai($id_san_pham, $size)) {
-                                // Sửa biến thể
-                                sua_bien_the_san_pham($id_san_pham, $size, $gia_nhap, $gia_ban, $sale, $so_luong);
-                            } else {
-                                // Thêm biến thể mới
-                                them_bien_the_san_pham($id_san_pham, $size, $gia_nhap, $gia_ban, $sale, $so_luong);
-                            }
-                        }
-                
-                          // Xử lý upload ảnh
-        $numFiles = count($_FILES['productImages']['name']);
-        $uploadedImages = array();
-
-        for ($i = 0; $i < $numFiles; $i++) {
-            $image_name = $_FILES['productImages']['name'][$i];
-            $tmp = $_FILES['productImages']['tmp_name'][$i];
-            $uploadPath = '../uploads/' . $image_name;
-
-            if (move_uploaded_file($tmp, $uploadPath)) {
-                $uploadedImages[] = $image_name; // Lưu tên ảnh vào mảng để thêm vào cơ sở dữ liệu sau này
-            } else {
-                echo "Không thể tải lên ảnh $image_name.<br>";
-            }
-        }
-
-        // Thêm ảnh vào db
-        foreach ($uploadedImages as $image) {
-            them_anh_san_pham($id_san_pham, $image);
-        }
-                
-                        // Thông báo thành công
-                        $thongbao = "Thêm sản phẩm thành công";
-                        header('Location:index.php?act=san_pham');
-                    }
                     break;
             case 'giam_gia':
          break;
@@ -434,14 +158,68 @@ if(isset($_GET['act'])){
            case 'them_ma':
           break;
         case 'tai_khoan':
+       $all_tai_khoan = show_tai_khoan();
+       include 'view/taikhoan/list.php';
          break;
           case 'them_tai_khoan':
+       $loi_ten = $loi_email = $loi_mat_khau  = "";
+                $dem = 0;
+                if(isset($_POST['them_tk_btn']) ){
+                    $ten_dang_nhap = $_POST['ten_tai_khoan'];
+                    $email = $_POST['email'];
+                    $mat_khau = $_POST['mat_khau'];       
+                    $trang_thai = 0;
+                    $vai_tro = 1;
+                    if (isset($_FILES['anh_dai_dien']) && $_FILES['anh_dai_dien']['error'] == 0) {
+                        $hinh_anh = $_FILES['anh_dai_dien']['name'];
+                        $tmp = $_FILES['anh_dai_dien']['tmp_name'];
+                        move_uploaded_file($tmp, '../uploads/' . $hinh_anh);
+                    } else {
+                        $hinh_anh = 'default.jpg'; // Ảnh mặc định nếu không upload
+                    }
+                    #xử lý validate
+                    if(empty($ten_dang_nhap) ){
+                        $loi_ten = "Không được để trống tên đăng nhập";
+                        $dem ++;
+                    }
+                    if(empty($email) ){
+                        $loi_email = "Không được để trống email";
+                        $dem ++;
+                    }
+                    if(empty($mat_khau) ){
+                        $loi_mat_khau = "Không được để trống mật khẩu";
+                        $dem ++;
+                    }
+               
+
+                    #tiến hành thêm tài khoản nếu không xảy ra lỗi
+                    if($dem == 0){
+                        them_tai_khoan($ten_dang_nhap, $mat_khau, $email, $trang_thai, $hinh_anh, null, null, $vai_tro) ;
+                        $thongbao ="Thêm tài khoản thành công";
+                    }
+                 
+
+                }
+                include 'view/taikhoan/add.php';
             break;
         case 'xoa_tai_khoan':
+        if(isset($_GET['id_tk'])){
+                $id_tai_khoan = $_GET['id_tk'];
+                block_tai_khoan($id_tai_khoan);
+            }
+            header('Location:index.php?act=tai_khoan');
+       
             break;
          case 'tai_khoan_da_khoa':
+       $all_tai_khoan_khoa = show_tai_khoan_bi_khoa();
+                include 'view/taikhoan/listkhoa.php';
             break;
          case 'khoi_phuc_tai_khoan':
+       if(isset($_GET['id_tk'])){
+                    $id_tai_khoan = $_GET['id_tk'];
+                    khoi_phuc_tai_khoan($id_tai_khoan);
+                }
+                header('Location:index.php?act=tai_khoan_da_khoa');
             break;
           case 'don_hang':
             break;
@@ -450,35 +228,33 @@ if(isset($_GET['act'])){
          case 'thay_doi_trang_thai_don':
             break;
         case 'binh_luan':
+        $binh_luan =show_binh_luan();
+                include 'view/binhluan/list.php.';
             break;
               case 'xem_binh_luan':
+        if(isset($_GET['id_binh_luan'])){
+                    $id_binh_luan = $_GET['id_binh_luan'];
+                    $one_binh_luan = show_chi_tiet_binh_luan($id_binh_luan);
+                }
+                include 'view/binhluan/chitiet.php.';
             break;
           case 'an_binh_luan':
+       if(isset($_GET['id_binh_luan'])){
+                    an_binh_luan($_GET['id_binh_luan']);
+                }
+                header('Location:index.php?act=binh_luan');
             break;
         case 'binh_luan_da_xoa':
+       $binh_luan_da_xoa =show_binh_luan_da_xoa();
+                    include 'view/binhluan/list_delete.php.';
             break;
         default:
-           $chua_xu_ly = dem_don_chu_xu_ly();
-        $da_xu_ly = dem_don_da_xu_ly();
-        $da_giao =  dem_don_da_giao();
-        $da_huy = dem_don_da_huy();
-        $san_pham_da_xoa = san_pham_da_khoa();
-        $san_pham_het_hang =san_pham_het_hang();
-        include 'view/main.php';
-         include 'view/bieudodoanhthu.php';
-            break;
+           include 'view/main.php';
+         break;
     }
 }else{
-      $chua_xu_ly = dem_don_chu_xu_ly();
-            $da_xu_ly = dem_don_da_xu_ly();
-            $da_giao =  dem_don_da_giao();
-            $da_huy = dem_don_da_huy();
-            $san_pham_da_xoa = san_pham_da_khoa();
-            $san_pham_het_hang =san_pham_het_hang();
-     include 'view/bieudodoanhthu.php';
-    include 'view/main.php';
+     include 'view/main.php';
 }
-
 include 'view/footer.php';
 ob_end_flush();
 ?>
